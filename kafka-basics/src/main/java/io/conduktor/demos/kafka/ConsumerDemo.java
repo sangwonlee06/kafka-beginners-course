220f7c927f834daa6183536a8961ccc5ba4db345
sangwonlee06 <sangwonlee0622@outlook.com>
2026-03-21 19:59:47 -0700

    feat: add consumer demo with graceful shutdown handling

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -19,7 +19,7 @@ public class ConsumerDemo {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
     public static void main(String[] args) {
-        log.info("Starting producer");
+        log.info("Starting consumer");
 
         String groupId = "my-java-application";
         String topic = "demo_java";
